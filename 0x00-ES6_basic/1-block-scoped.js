e5253e1dcd8959a9d45ddcb06622385c26434abd
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-06-25 01:22:20 +0300

    addpackge

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Use let for block-scoping
-  let task2 = true; // Use let for block-scoping
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;   // Assign new values directly without redeclaration
-    task2 = false; // Assign new values directly without redeclaration
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
--- 0x00-ES6_basic/unittest.test.js
+++
@@ -1,5 +0,0 @@
-import taskBlock from './1-block-scoped'; // Adjust the path if necessary
-
-test("say just message", () => {
-  expect(taskBlock(true)).toEqual([true, false]);
-});
