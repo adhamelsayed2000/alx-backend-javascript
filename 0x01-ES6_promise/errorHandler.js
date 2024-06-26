041c32783f8ecfe1ec0fac6be00e177b8a6b1852
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-06-27 00:56:31 +0300

    edit

--- 0x01-ES6_promise/errorHandler.js
+++ 0x01-ES6_promise/errorHandler.js
@@ -1,5 +1,4 @@
 export function handlePromiseError(error) {
-    console.error('Error occurred:', error.message);
-    process.exit(1); // تخرج من عملية Node.js برمز خطأ غير صفري
-  }
-  
+  console.error('Error occurred:', error.message);
+  process.exit(1);
+}
